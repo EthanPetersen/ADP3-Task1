7fb3239bafd42b1f0ee80665fadc762637489e80
Ethan <ethanpetersen10@gmail.com>
2020-02-18 01:02:23 +0200

    first commit

---
+++ Prac1/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    //TODO:Complete
+    public void todo(){
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int value1,value2,output;
+
+        System.out.println("Addition");
+        System.out.println("Please enter the first value:");
+        value1 = sc.nextInt();
+        System.out.println("Please enter the second value");
+        value2 = sc.nextInt();
+        output= value1 + value2;
+
+        System.out.println("Answer:" + output);
+    }
+}
